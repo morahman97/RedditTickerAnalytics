efe0ff2021e6ea541175af9d8d0537b5c96827c8
Mo Rahman <morahman97@gmail.com>
2021-02-20 23:02:43 -0800

    Improved overall appearance and bug fixes (consistent length arrays, invalid pathanme no longer crashes script)

--- dashtest.py
+++ dashtest.py
@@ -6,25 +6,36 @@ import dash_html_components as html
 from collections import deque, defaultdict, OrderedDict
 from dash.dependencies import Output, Input
 from requests import get
+import pandas as pd
+from datetime import datetime
 
 print(dcc.__version__) # 0.6.0 or above is required
 
-#external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+start_time = datetime.now()
+# Set up structure to hold ticker counts
+tickers = pd.read_csv("tickers.csv")
+tickers = set(tickers['Symbol'])
+database = defaultdict(list)
+for ticker in tickers:
+    database[ticker].append(0)
+
 X = []
 time = 0
 Y = []
-database = defaultdict(list)
 
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(
     [
         # represents the URL bar, doesn't render anything
         dcc.Location(id='url', refresh=False),
 
-        dcc.Link('Navigate to "/"', href='/'),
-        html.Br(),
-        dcc.Link('Navigate to "/page-2"', href='/page-2'),
+        html.H1('What\'s Ticking?'),
+
+#        dcc.Link('Navigate to "/"', href='/'),
+#        html.Br(),
+#        dcc.Link('Navigate to "/page-2"', href='/page-2'),
 
         # content will be rendered in this element
         html.Div(id='page-content'),
@@ -42,6 +53,17 @@ app.layout = html.Div(
               [dash.dependencies.Input('url', 'pathname'),
                Input('graph-update', 'n_intervals')])
 def update_graph_scatter(pathname, input_data):
+    # Handle invalid pathname input
+    if not pathname or pathname[1:] not in database:
+        data = plotly.graph_objs.Scatter(
+                x=[],
+                y=[],
+                name='Scatter',
+                mode= 'lines+markers'
+                )
+
+        return {'data': [data]
+        }
     global time
     update_data()
     X.append(time)
@@ -61,18 +83,17 @@ def update_graph_scatter(pathname, input_data):
                                                 yaxis=dict(range=[min(Y),max(Y)]),)}
 
 def update_data():
-    print(database)
+    #print(database)
     data = get('http://localhost:5000/').json()
     getkeys = data.keys()
     if data:
         print(data)
         for key,val in data.items():
-            if len(database[val[1]]) > 0:
-                database[val[1]].append(database[val[1]][-1] + 1)
-            else:
-                database[val[1]].append(1)
-        getkeys = set([i[1] for i in val])
+            database[val[1]].append(database[val[1]][-1] + 1)
+        getkeys = set([i[1] for i in data.values()])
+        print("GETKEYS TEST")
         print(getkeys)
+        print("END GETKEYS TEST")
     for key,val in database.items():
         if key not in getkeys:
             database[key].append(database[key][-1])
@@ -88,13 +109,14 @@ def update_data():
 #    for key,val in data.items():
 #        database[val[1]] += 1
     print("END")
-    print(database)
+    #print(database)
+    for key,val in database.items():
+        if 1 in val:
+            print(key, val)
     
 @app.callback(dash.dependencies.Output('page-content', 'children'),
               [dash.dependencies.Input('url', 'pathname')])
 def display_page(pathname):
-    print(type(pathname))
-    print(pathname)
     if pathname == "/":
         return html.Div([
             html.H3('You are on page {}'.format(pathname))
@@ -102,13 +124,12 @@ def display_page(pathname):
     query = pathname[1:]
     if query in database:
         return html.Div([
-            html.H3('Success: {}'.format(query))
+            html.H3('{0} as of {1}'.format(query, start_time))
         ])
     else:
         return html.Div([
-            html.H3('You are on page {}'.format(pathname))
+            html.H3('ERROR: Invalid Page (bug will break all graphs, restart server)')
         ])
-
 
 if __name__ == '__main__':
     app.run_server(debug=True)
